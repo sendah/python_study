87d00d7793705e6c2bcd0fc72ac89d2ecfd24da0
千田大貴 <hiroki@appurunoMacBook-puro.local>
2018-10-06 22:44:57 +0900

    diary作成

--- diary/urls.py
+++ diary/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'diary'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # /diary
+    path('', views.indexView.as_view(), name='index'),
+    path('add/', views.AddViews.as_view(), name='add'),  # /diary/add
+    path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),  # diary/update/1
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),  # /diary/delete/1
+    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),  # /diary/detail/1
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import DayCreateForm
+from .models import Day
 
-# Create your views here.
-def index(request):
-    return render(request, 'diary/day_list.html')
+class IndexView(generic.ListView):
+    model = Day
+
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')
+
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('diary:index')
+
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy('diary:index')
+
+class DetailView(generic.DetailView):
+    model = Day
