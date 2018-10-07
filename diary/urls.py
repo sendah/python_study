dc11a65ac23aa0860c4267f10d152216a421eb95
千田大貴 <hiroki@appurunoMacBook-puro.local>
2018-10-07 11:43:59 +0900

    管理者機能追加

--- diary/urls.py
+++ diary/urls.py
@@ -5,8 +5,8 @@ app_name = 'diary'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('add/', views.AddView.as_view(), name='add'),  # /diary/add
-    path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),  # diary/update/1
-    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),  # /diary/delete/1
+    #path('add/', views.AddView.as_view(), name='add'),  # /diary/add
+    #path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),  # diary/update/1
+    #path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),  # /diary/delete/1
     path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),  # /diary/detail/1
 ]
