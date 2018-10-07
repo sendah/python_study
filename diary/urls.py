5cb7b7820299e9e0862e1f5229a65143fc6f56e3
千田大貴 <hiroki@appurunoMacBook-puro.local>
2018-10-07 11:08:13 +0900

    バグ修正

--- diary/urls.py
+++ diary/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'diary'
 
 urlpatterns = [
-    path('', views.indexView.as_view(), name='index'),
-    path('add/', views.AddViews.as_view(), name='add'),  # /diary/add
+    path('', views.IndexView.as_view(), name='index'),
+    path('add/', views.AddView.as_view(), name='add'),  # /diary/add
     path('update/<int:pk>/', views.UpdateView.as_view(), name='update'),  # diary/update/1
     path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),  # /diary/delete/1
     path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),  # /diary/detail/1
--- diary/views.py
+++ diary/views.py
@@ -6,6 +6,7 @@ from .models import Day
 
 class IndexView(generic.ListView):
     model = Day
+    pagenate_by = 3
 
 class AddView(generic.CreateView):
     model = Day
